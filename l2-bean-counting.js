d0392b55454183e6c35a0259de1ed3742dba28bd
ramon96 <rfm1@live.nl>
2019-04-17 16:16:57 +0200

    did an bonus. did B counting
    Still having fun

--- l2-bean-counting.js
+++ l2-bean-counting.js
@@ -1,1 +1,19 @@
- 
+ function countBs(Word){
+     let bAmount = 0;
+     for(let i=0; i < Word.length; i++){
+        if(Word[i] == "B"){
+            bAmount += 1;
+        }
+     }
+     return bAmount;
+ }
+
+ function countChar(Word, Character){
+    let charAmount = 0;
+    for(let i=0; i < Word.length; i++){
+        if(Word[i] == Character){
+            charAmount += 1;
+        }
+     }
+     return charAmount;
+ }
--- l2-minimum.js
+++ l2-minimum.js
@@ -7,3 +7,19 @@ function min(value1, value2){
         return value1;
     }
  }
+
+
+ // recursion for the bonus :)
+
+ function isEven(N){
+    if(N==0){
+        return true;
+    }
+    else if (N==1){
+        return false;
+    }
+    else{
+        N -= 2;
+        return isEven(N);
+    }
+ }
