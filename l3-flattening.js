a2bb114ee50f34d82048d405a7d32304e9262086
ramon96 <rfm1@live.nl>
2019-04-19 20:18:33 +0200

    understand concat a little better now

--- l3-flattening.js
+++ l3-flattening.js
@@ -1,7 +1,7 @@
 let arrays = [[1, 2, 3], [4, 5], [6]];
 // Your code here.
 const flatten = function(array, value){
-  const result = array.concat("") + value.concat("");
+  const result = array.concat(value);
   return result;
 }
 
