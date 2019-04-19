811b8b1fc1e76d6e051c257a3d824de326199f6d
ramon96 <rfm1@live.nl>
2019-04-19 13:23:58 +0200

    no message

--- l3-flattening.js
+++ l3-flattening.js
@@ -1,1 +1,8 @@
- 
+let arrays = [[1, 2, 3], [4, 5], [6]];
+// Your code here.
+const flatten = function(array, value){
+  const result = array.concat("") + value.concat("");
+  return result;
+}
+
+console.log(arrays.reduce(flatten));
