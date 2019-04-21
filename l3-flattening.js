60bf3b10af5655fd4a1b0ae5998b493060bb1509
Ramon Meijers <rfm1@live.nl>
2019-04-21 21:26:06 +0200

    no message

--- l3-flattening.js
+++ l3-flattening.js
@@ -1,1 +1,10 @@
- 
+function flatten(array, sum) {
+	return array + sum.concat("");
+}
+
+
+let arrays = [[1, 2, 3], [4, 5], [6]];
+// Your code here.
+let result = arrays.reduce(flatten, []);
+console.log(result)
+// → [1, 2, 3, 4, 5, 6] 
